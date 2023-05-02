d8d6142caafa98e7698825114e84deeff92773d3
mh akash <mhakash5000@gmail.com>
2023-05-02 08:09:32 +0600

    footer added

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            Footer
+  return (
+    <footer className="bg-black py-4">
+      <div className="container mx-auto flex flex-col sm:flex-row items-center justify-between">
+        <div className="text-white text-center sm:text-left mb-2 sm:mb-0">
+          <p>&copy; 2023 All rights reserved here.</p>
         </div>
-    );
+        <div className="text-white text-center">
+          <p>Designed and Developed by CodingDuck</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
